649d0807ce82c398b6de20a73c929f61fe7eb611
Drew <dtatkison@gmail.com>
2023-04-29 13:59:40 -0500

    implemented projects

---
+++ apps/faith-giving-ui/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "faith-giving-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent {
+  title = "faith-giving-ui";
+}
