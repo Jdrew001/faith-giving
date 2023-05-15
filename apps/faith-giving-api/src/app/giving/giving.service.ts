c57a3531eadd45939eac04da28aa770e059ff441
Drew Atkison <dtatkison@gmail.com>
2023-05-14 21:00:56 -0500

    added giving report

--- apps/faith-giving-api/src/app/giving/giving.service.ts
+++ apps/faith-giving-api/src/app/giving/giving.service.ts
@@ -4,13 +4,16 @@ import { CreatePaymentIntentDto } from '../dto/giving/create-payment-intent.dto'
 import { StripeService } from '../services/stripe.service';
 import { GiveDetails, PaymentDTO } from '../dto/giving/payment.dto';
 import { getDocs, where, query, addDoc } from 'firebase/firestore';
+import { EmailService } from '../services/email/email.service';
+import { EmailConstant } from '../services/email/email.constant';
 
 @Injectable()
 export class GivingService {
 
     constructor(
         private dataService: DataService,
-        private stripeService: StripeService
+        private stripeService: StripeService,
+        private emailService: EmailService
     ) {}
 
     async createPaymentIntent(body: CreatePaymentIntentDto) {
@@ -25,12 +28,19 @@ export class GivingService {
         } catch (error) {
             throw new BadRequestException('An error occurred', { cause: new Error(), description: 'error submitting payment' });
         }
-       
-
+        
         if (payment.status == 'succeeded') {
+            Logger.log('Begin transaction of giving information')
             let uploadResult = await this.uploadGivingInformation(body.giveDetails);
             if (!uploadResult) {
                 Logger.error(`Giving information upload failed`);
+            } else {
+                //Send give report to pastor
+                Logger.log('Sending email to admins')
+                await this.emailService.sendEmailToTemplate<any>("dtatkison@gmail.com", EmailConstant.GIVING_REPORT, this.generateGivingReport(body.giveDetails));
+
+                //Send give recept to giver
+                
             }
         }
         return payment;
@@ -59,5 +69,18 @@ export class GivingService {
         }
 
         return {data: data.docs.map(doc => doc.data()) as GiveDetails[], length: data.docs.length};
+    }
+
+    private generateGivingReport(data: GiveDetails) {
+        let offerings = '';
+        data.offerings.forEach(item => {
+            offerings += `${item.category}: $${item.amount}\n`
+        });
+        return {
+            fullName: `${data.firstName} ${data.lastName}`,
+            email: data.email,
+            tithe: data.tithe,
+            offerings: offerings
+        }
     }
 }
--- apps/faith-giving-api/src/app/services/email/email.constant.ts
+++ apps/faith-giving-api/src/app/services/email/email.constant.ts
@@ -1,4 +1,5 @@
 export class EmailConstant {
     public static HELLO_WORLD_TEMPLATE = 'd-dd683b51061d43bdbff208fd08b91428';
     public static SEND_GRID_URL = 'https://api.sendgrid.com/v3/mail/send';
+    public static GIVING_REPORT = 'd-cfb29777cf724dbd830bc304cf00b803';
 }
