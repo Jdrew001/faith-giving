9c8526f1c83a2dfaf649635819df1f9ef420aed1
Drew Atkison <dtatkison@gmail.com>
2023-06-03 22:02:02 -0500

    mock data added

--- apps/faith-giving-api/src/app/export/export.service.ts
+++ apps/faith-giving-api/src/app/export/export.service.ts
@@ -5,11 +5,89 @@ import { GivingSheet } from './templates/giving-excel';
 @Injectable()
 export class ExportService {
 
+    mockData = {
+        "data": [
+            {
+                "name": "January",
+                "id": 1,
+                "weeks": ["", "", "$100.00", "", ""],
+                "total": "$100.00"
+            },
+            {
+                "name": "February",
+                "id": 2,
+                "weeks": ["", "", "$100.00", "", ""],
+                "total": "$100.00"
+            },
+            {
+                "name": "March",
+                "id": 3,
+                "weeks": ["", "", "$100.00", "", ""],
+                "total": "$100.00"
+            },
+            {
+                "name": "April",
+                "id": 4,
+                "weeks": ["", "", "$100.00", "", ""],
+                "total": "$100.00"
+            },
+            {
+                "name": "May",
+                "id": 5,
+                "weeks": ["", "", "$100.00", "", ""],
+                "total": "$100.00"
+            },
+            {
+                "name": "June",
+                "id": 6,
+                "weeks": ["", "", "$100.00", "", ""],
+                "total": "$100.00"
+            },
+            {
+                "name": "July",
+                "id": 7,
+                "weeks": ["", "", "$100.00", "", ""],
+                "total": "$100.00"
+            },
+            {
+                "name": "August",
+                "id": 8,
+                "weeks": ["", "", "$100.00", "", ""],
+                "total": "$100.00"
+            },
+            {
+                "name": "September",
+                "id": 9,
+                "weeks": ["", "", "$100.00", "", ""],
+                "total": "$100.00"
+            },
+            {
+                "name": "October",
+                "id": 10,
+                "weeks": ["", "", "$100.00", "", ""],
+                "total": "$100.00"
+            },
+            {
+                "name": "November",
+                "id": 11,
+                "weeks": ["", "", "$100.00", "", ""],
+                "total": "$100.00"
+            },
+            {
+                "name": "December",
+                "id": 12,
+                "weeks": ["", "", "$100.00", "", ""],
+                "total": "$100.00"
+            }
+        ],
+        "total": "$100.00"
+    }
+
     generateGivingExcel() {
         let result;
         try {
             const workbook = XLSX.utils.book_new();
-            const template = new GivingSheet();
+            const template = new GivingSheet().generateSheet();
     
             XLSX.utils.book_append_sheet(workbook, template, 'Sheet1');
             result = XLSX.write(workbook, { type: 'buffer', bookType: 'xlsx' });
