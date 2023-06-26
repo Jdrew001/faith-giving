da3f8753a46d464c9eef8cc3e492146f05272871
Drew <dtatkison@gmail.com>
2023-06-26 17:02:45 -0500

    update

---
+++ apps/faith-giving-api/migrations/1687816537934-testing_migration.ts
@@ -0,0 +1,12 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class TestingMigration1687816537934 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        queryRunner.
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
