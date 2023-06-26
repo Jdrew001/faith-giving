0d20d31b16f6a0c7446b45c20971c992b0ec97f6
Drew Atkison <dtatkison@gmail.com>
2023-06-26 17:10:29 -0500

    migration added

--- apps/faith-giving-api/migrations/1687816537934-testing_migration.ts
+++ apps/faith-giving-api/migrations/1687816537934-testing_migration.ts
@@ -3,7 +3,10 @@ import { MigrationInterface, QueryRunner } from "typeorm"
 export class TestingMigration1687816537934 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        queryRunner.
+        queryRunner.query(`
+            INSERT into role (id)
+            values (4);
+        `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
