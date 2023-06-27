f865a35c4bfbd546ccab697358b3e366ce1b37ee
Drew <dtatkison@gmail.com>
2023-06-27 00:16:10 -0500

    updated script

--- scripts/initdb.sql
+++ scripts/initdb.sql
@@ -1,5 +1,4 @@
-
--- Ref Data additions --
+-- Ref Data --
 insert into offering_type (id, label, disabled, "activeInd")
     values (1, 'General Offering', false, 'A');
 
@@ -22,27 +21,20 @@ insert into offering_type (id, label, disabled, "activeInd")
     values (7, 'Other', false, 'A');
 
 -- Role Data Additions
-insert into role (id)
-    values (0);
+insert into role (id, name)
+    values (0, 'ADMIN');
 
-insert into role (id)
-    values (1);
+insert into role (id, name)
+    values (1, 'VIEWER');
 
-insert into "user" (email, firstname, lastname, phonenumber)
-    values ('dtatkison@gmail.com', 'Drew', 'Atkison', '6824140386');
+insert into "user" (id, email, firstname, lastname, phonenumber)
+    values ('be7b7910-0e52-11ee-be56-0242ac120002', 'dtatkison@gmail.com', 'Drew', 'Atkison', '6824140386');
 
-insert into "user" (email, firstname, lastname, phonenumber)
-    values ('elatkison@gmail.com', 'Elizabeth', 'Atkison', '8177186462');
+insert into "user" (id, email, firstname, lastname, phonenumber)
+    values ('39ba9030-9859-4621-803c-c3ebac4feb29', 'elatkison@gmail.com', 'Elizabeth', 'Atkison', '8177186462');
 
--- insert into user_roles_role ("userId", "roleId")
---     values ('be7b7910-0e52-11ee-be56-0242ac120002', 0);
---
--- insert into user_roles_role ("userId", "roleId")
---     values ('39ba9030-9859-4621-803c-c3ebac4feb29', 1);
+insert into user_roles_role ("userId", "roleId")
+    values ('be7b7910-0e52-11ee-be56-0242ac120002', 0);
 
-
-
---
--- select * from role;
--- select * from offering_type;
--- select * from "user";
+insert into user_roles_role ("userId", "roleId")
+    values ('39ba9030-9859-4621-803c-c3ebac4feb29', 1);
